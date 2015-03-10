6546e3ed9955b4faf86fff9db38a07677b521da8
David Farr <david.farr@sap.com>
2015-03-10 15:48:41 -0700

    Update

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -2,8 +2,13 @@
 //= require vendor/bootstrap.min
 
 // animate scroll to anchor
-$("a[href^=#]").click(function(e) {
+$('a[href^=#]').click(function(e) {
     e.preventDefault(); 
     var dest = $(this).attr('href'); 
     $('html,body').animate({ scrollTop: $(dest).offset().top }, 300); 
+});
+
+// hide the "more" link 
+$('a[href*=#more]').click(function(e) {
+    $(e.target.parentElement).hide();
 });
